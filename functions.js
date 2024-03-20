8cb7878901045515d0a9c8368e65433f2073e89f
Ericcyyyy <117465025+Ericcyyyy@users.noreply.github.com>
2024-03-20 12:59:47 -0700

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,13 @@
+// functions.js
+// Eric Yo
+// 3/20/2024
+
+var sidemenu = document.getElementById("sidemenu")
+
+function openMenu() {
+    sidemenu.style.right = "0";
+}
+
+function closeMenu() {
+    sidemenu.style.left = "-200px";
+}
