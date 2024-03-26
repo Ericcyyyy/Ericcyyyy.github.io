f50687d000d7550dd4e016d6c9076d33a2232a85
Eric Yo <eric.michael.yo@gmail.com>
2024-03-26 14:36:05 -0700

    Fixed nav bar

--- functions.js
+++ functions.js
@@ -2,12 +2,17 @@
 // Eric Yo
 // 3/20/2024
 
-var sidemenu = document.getElementById("sidemenu")
+document.addEventListener("DOMContentLoaded", function() {
+    var sidemenu = document.getElementById("sidemenu")
 
-function openMenu() {
-    sidemenu.style.right = "0";
-}
+    function openMenu() {
+        sidemenu.style.right = "0";
+    }
 
-function closeMenu() {
-    sidemenu.style.left = "-200px";
-}
+    function closeMenu() {
+        sidemenu.style.left = "-200px";
+    }
+
+    document.querySelector(".fa-bars").addEventListener("click", openMenu);
+    document.querySelector(".fa-times").addEventListener("click", closeMenu);
+});
